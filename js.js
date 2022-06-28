234bb97355c8b711e378a458f173d2bea2270a16
muhammadbarik26 <mhdbarik26@gmail.com>
2022-06-28 08:57:03 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+let contact = document.getElementById("contact");
+contact.addEventListener("click", function contact(){
+    alert("Hubungi saya melalui media sosial");
+})
+
+//https://github.com/dzulfikarnurfikri/webportfolio1
